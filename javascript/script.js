6074019d8d5f363d70588311a87fc8c990db729c
Alisson Peloso <alisson.luan2000@gmail.com>
2021-04-01 14:37:03 -0300

    add chat and whatsapp button

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,18 @@ function clkmenu(x) {
     x.classList.toggle("change");
     document.getElementById("nav").classList.toggle("show");
 }
+
+function open_chat(element) {
+    element.classList.add("hide");
+    document.getElementById("chat").classList.remove("closed");
+    document.getElementById("chat").classList.remove("minimized");
+}
+
+function close_chat(){
+    document.getElementById("chat_button").classList.remove("hide");
+    document.getElementById("chat").classList.add("closed");
+}
+
+function minimize_chat(){
+    document.getElementById("chat").classList.toggle("minimized");
+}
